df0fa6284589d6ab3c4df938a4499ac9242de5c3
Shikhar Sharma <shikhar38@gmail.com>
2013-11-14 16:47:57 +0530

    removed older codes

--- Code 2 - Copy/read_demography.m
+++
@@ -1,2 +0,0 @@
-function dataset=read_demograpgy(filename);
-dataset=textscan(filename,'%u %u %s %s %s','Delimiter','|')
--- Code/computeRatings.m
+++
@@ -1,2 +0,0 @@
-dataset=read_dataset('../ml-100k/u.data',0);
-rating=find_rating(1,12,dataset,similarity,20);
